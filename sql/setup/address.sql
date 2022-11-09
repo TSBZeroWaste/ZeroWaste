e99040ec07583808115617f874ba1548957a2b00
ryu-tsb <117259294+ryu-tsb@users.noreply.github.com>
2022-11-09 17:56:32 +0900

    Create address.sql

---
+++ sql/setup/address.sql
@@ -0,0 +1,8 @@
+create table if not exists zw_address(
+    code int(6) not null,
+    ken varchar(255) not null,
+    shi_ku varchar(255) not null,
+    banchi varchar(255) not null,
+    building varchar(255) not null,
+    room varchar(255) not null
+);
