6a920c6e2506fbb105c8ec4ac582c36be51dd091
ryu-tsb <117259294+ryu-tsb@users.noreply.github.com>
2022-11-09 17:54:55 +0900

    Delete setup directory

--- setup/address.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists zw_address(
-    code int(6) not null,
-    ken varchar(255) not null,
-    shi_ku varchar(255) not null,
-    banchi varchar(255) not null,
-    building varchar(255) not null,
-    room varchar(255) not null
-);
--- setup/collection.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists zw_collection(
-    collection_id int(11) not null auto_increment,
-    collection_kj int(255) not null,
-    category int not null,
-    deleted_ku varchar(1) not null comment '0: 正常; 1:削除済み',
-    insert_time datetime not null default current_timestamp comment '登録日時',
-    update_time datetime not null default current_timestamp comment '更新日時',
-    primary key(collection_id)
-);
