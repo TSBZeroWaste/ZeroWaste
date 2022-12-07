2affae0ded5a976f5b7a6e7c0c07d383dbeb5704
ryu-tsb <117259294+ryu-tsb@users.noreply.github.com>
2022-12-07 15:19:24 +0900

    Update init.php

--- sql/init.php
+++ sql/init.php
@@ -2,21 +2,25 @@
 require_once(__DIR__.'/Setup.php');
 
 $pass = $_POST['pass'];
-$db_num = $_POST['db_num'];
+if (pass == 1024) {
+    $db_num = $_POST['db_num'];
 
-$setup = Setup();
+    $setup = Setup();
 
-switch ($db_num) {
-    case 0:
-        $setup->connectToRemote();
-        $setup->initDB();
-        break;
-    case 1:
-        $setup->connectToLocal();
-        $setup->initDB();
-        break;
-    default:
-        echo "<h2>無効な値が入力されました＾＾</h2>";
+    switch ($db_num) {
+        case 0:
+            $setup->connectToRemote();
+            $setup->initDB();
+            break;
+        case 1:
+            $setup->connectToLocal();
+            $setup->initDB();
+            break;
+        default:
+            echo "<h2>DB番号に無効な値が入力されました＾＾</h2>";
+    }
+} else {
+    echo "<h2>パスコードに無効な値が入力されました＾＾</h2>";
 }
 ?>
 
@@ -28,8 +32,8 @@ switch ($db_num) {
 </head>
 <body>
     <form action="init.php" method="post">
-    <p><input type="text" name="pass"></p>
-    <p><input type="text" name="db_num"></p>
+    <p>パスコード<input type="text" name="pass"></p>
+    <p>DB番号<input type="text" name="db_num"></p>
     <input type="submit" value="削除実行">
     </form>
 </body>
